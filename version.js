4f43f7fb55127bef5422166167fbe061f750e2f3
William J Davis <william.j.davis5@gmail.com>
2016-08-13 23:53:06 -0400

    a few quick updates to fix version patching.

--- version.js
+++ version.js
@@ -1,11 +1,14 @@
 var jsonfile = require('jsonfile');
 var util = require('util');
 
-var file = '../src/RabbitMqConsumers/project.json';
+var file = 'src\\RabbitMqConsumers\\project.json';
 var buildNumber = process.env.APPVEYOR_BUILD_VERSION;
 
 jsonfile.readFile(file, function (err, project) {
+    //console.log(project);
+    //console.log(err);
     project.version = buildNumber;
+    
     jsonfile.writeFile(file, project, { spaces: 2 }, function (err) {
         console.error(err);
     });
